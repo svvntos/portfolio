c41b28a6fa08e6857561d3a6a45865e26a68d99c
Joe Santos <jaysantos012@gmail.com>
2021-04-20 02:16:22 -0400

    finished the landing page with profile image, nav bar, and social links. Now working on button to download m resume and start working on about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,10 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
+// show scroll Top
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+
