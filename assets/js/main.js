d7d85716283ff7b556ab95a8376f4d9011dcdfea
Joe Santos <jaysantos012@gmail.com>
2021-04-19 22:38:07 -0400

    updating the nav bar. making sure the toggle works and showing the proper active one selected

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,3 +44,11 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
+// change background header
+
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
